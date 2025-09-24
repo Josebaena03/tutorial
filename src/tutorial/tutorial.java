4f6795b0f60758f8a92092fc41d5a688c9b5edc4
Josebaena03 <josecolegioo12@gmail.com>
2025-09-24 12:32:15 +0200

    Invoca al método de despedida

--- src/tutorial/tutorial.java
+++ src/tutorial/tutorial.java
@@ -4,6 +4,7 @@ public class tutorial {
 	public static void main(String[] args) {
 		System.out.print("Hola");
 		System.out.println("Este es un saludo especial desde la rama feature/saludo-especial.");
+		despedirse();
 	}
 	
 	 public static void despedirse() {
